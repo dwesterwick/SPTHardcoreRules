fa1e41a3642b52c7a3879d36e0438e8f9b704fec
dwesterwick <dwesterwick@yahoo.com>
2023-03-19 14:36:38 -0400

    Created visual studio project

---
+++ bepinex_dev/SPTHardcoreRules/SPTHardcoreRulesPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPTHardcoreRules
+{
+    [BepInPlugin("com.DanW.SPTHardcoreRules", "SPTHardcoreRulesPlugin", "1.1.0.0")]
+    public class SPTHardcoreRulesPlugin : BaseUnityPlugin
+    {
+        public static ISession CurrentSession { get; set; } = null;
+
+        private void Awake()
+        {
+            Logger.LogInfo("Loading SPTHardcoreRulesPlugin...");
+
+            Logger.LogInfo("Loading SPTHardcoreRulesPlugin...done.");
+        }
+    }
+}
