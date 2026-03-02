bf52960fadd148c26d502d5de571f5c938483f8e
Dan Westerwick <dwesterwick@yahoo.com>
2026-03-01 23:08:12 -0500

    Updated license in ModMetadata

--- Server/ModMetadata.cs
+++ Server/ModMetadata.cs
@@ -14,7 +14,7 @@ namespace HardcoreRules
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
         public override string? Url { get; init; }
         public override bool? IsBundleMod { get; init; } = false;
-        public override string License { get; init; } = "MIT";
+        public override string License { get; init; } = "CC BY-NC-SA 4.0";
 
         public string RelativePathToSptInstall = ModInfo.RELATIVE_PATH_TO_SPT_INSTALL;
     }
